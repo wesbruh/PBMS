c793e05096f389303eb49ddbea25aa9fee21696c
luis-pds <luispedrodesantiago@icloud.com>
2025-10-29 07:32:58 -0700

    PBMS 70 Add Login and Create Account Buttons

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -2,10 +2,18 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className=''>
+    <nav className= "w-full flex flex-wrap items-center gap-2.5 px-2 pr-2 py-3">
       <Link to="/">Home</Link> |{" "}
       <Link to="/about">About</Link> |{" "}
       <Link to="/testimonials">Testimonials</Link>
+      <Link
+       to="/login"
+       className= "ml-auto shrink-0 inline-block px-4 py-1.5 bg-brown text-white text-ms leading-tight font-serif hover:bg-[#AB8C4B] transition border-2 border-black rounded-lg"
+      >Log in</Link>
+      <Link
+       to="/signup"
+       className="ml-2.5 shrink-0 inline-block px-4 py-1.5 bg-brown text-white text-ms leading-tight font-serif hover:bg-[#AB8C4B] transition border-2 border-black rounded-lg"
+      >Create account</Link>
     </nav>
   );
 }
