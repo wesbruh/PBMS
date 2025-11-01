51d4e1a99529fd972593e8d8fd2585a98922883e
wesbruh <westleyv77@gmail.com>
2025-11-01 00:37:00 -0700

    PBMS-12 - As a visitor, I want to view a professional home page introducing Your Roots Photography so that I can understand the brand and its services.
    SUBTASKS:
    PBMS-64:
    Update: Fixed Navbar bugs with sizing of text and formatting across screen zoom size.
    
    PBMS-65: Add opening image to background of top section of Home Page
    Used image from client, contains "brand like" message.
    
    PBMS-66: Add section that include carousel of photos
    Implemented a CarouselSection component to be imported in HomePage.jsx.
    This component is located in the /components/HomePageComps folder.
    Followed best practice for React as this carousel component can be reused in the future if needed.
    Book With Me! Button correctly navigates to contact page.
    This carousel is in section 3 of the home page.
    
    PBMS-67: Add another section that has small "about me" information of the photographer.
    Designed brief about me in section 2 of the home page. Use appropriate stying.
    Reused svg curly design from 'About' page with some added modifications to make it more unique.
    
    PBMS-68: "Book with me button" or "Contact me" button
    As mentioned, this button is overlayed on the image carousel.
    Correctly navigates to Book With Me/Contact page.
    
    PBMS-69: Add footer to website
    Footer was already implemented as a place holder.
    Updated the design to be sized appropriately.
    Added social media icons and centered in the footer.
    Social media icons navigate to designated profiles when clicked on.
    
    !!!
    index.html:
    Added Font Awesome 6.4.0 CDN link to index.html to enable social media icons (Instagram, Facebook, TikTok) in the footer component.
    This allows us to use Font Awesome's icon library without requiring npm installation in our project.
    
    Added more photos for use in /public/images folder.

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -4,27 +4,29 @@ function Navbar() {
   // determines location of user based on current page
   const location = useLocation();
   // helper functions to indicate if the page link is an active page, including special services tab
+  // if on homePage navbar is transparent
   const isActive = (path) => location.pathname === path;
   const isSpecialServicesActive = location.pathname.startsWith('/services/weddings') || location.pathname.startsWith('/services/labor-and-delivery');
+  const isHomePage = location.pathname === '/';
 
   // shared styles for navbar links, sweeping underline when hovered with alternate color. Static underline with bg-brown
-  const linkStyles = "tracking-wide relative after:content-[''] after:absolute after:bottom-[-4px] after:left-0 after:w-0 after:h-[2px] after:bg-[#AB8C4B] hover:after:w-full after:transition-all after:duration-75 after:ease-in";
+  const linkStyles = `tracking-wide relative after:content-[''] after:absolute after:bottom-[-4px] after:left-0 after:w-0 after:h-[2px] after:bg-[#AB8C4B] hover:after:w-full after:transition-all after:duration-500 after:ease-in' ${isHomePage ? 'text-white' : 'text-black'}`;
   const activeLinkStyles = "after:!w-full after:bg-brown after:h-[2px]";
 
   return (
-    // each link uses the styles according to user indicating hover over a tab and what the active page
-    <nav className= 'w-full flex items-center px-12 pr-15 py-3'>
+    // each link uses the styles according to user indicating hover over a tab and what the active page, navbar is trasnparent on homepage only
+    <nav className={`w-full flex items-center pt-[2%] p-4  ${isHomePage ? 'bg-transparent absolute top-0 left-0 z-20' : 'bg-[#FFFDF4]'}`}>
 
       {/* logo and brand name on left side of Navbar */}
       <div className='flex items-center gap-3'>
         <img src="public/favicon.ico" alt="Your Roots Photography Logo" className='h-12 w-12'/>
-        <h1 className='text-[#7E4C3C] font-serif text-3xl felx-grid'>Your Roots<br />Photography</h1> 
+        <h1 className={`${isHomePage ? 'text-white' : 'text-[#7E4C3C]'} font-serif text-2xl flex-grid`}>Your Roots Photography</h1> 
       </div>
 
       {/* Navbar tabs and buttons on right side of Navbar. 
       specifically designed to have gaps between tabs and not large gaps between the buttons */}
-      <div className='flex font-serif text-lg items-center ml-auto'>
-        <div className='flex items-center gap-10'>
+      <div className='flex font-mono text-sm  items-center ml-auto'>
+        <div className='flex items-center gap-6'>
       <Link to="/" className={`${linkStyles} ${isActive('/') ? activeLinkStyles : ''}`}>Home</Link> 
       <Link to="/about" className={`${linkStyles} ${isActive('/about') ? activeLinkStyles : ''}`}>About</Link> 
       <Link to="/testimonials" className={`${linkStyles} ${isActive('/testimonials') ? activeLinkStyles : ''}`}>Testimonials</Link>
@@ -37,10 +39,10 @@ function Navbar() {
         <ul className ='absolute top-full mt-2 bg-white shadow-xl rounded-md py-2 z-10 max-h-0 opacity-0 overflow-hidden 
         group-hover:max-h-40 group-hover:opacity-100  transition-all duration-800 ease-out'>
           <li className='px-3 py-1'>
-          <Link to="/services/weddings" className={`${linkStyles} ${isActive('/services/weddings') ? activeLinkStyles : ''}`}>Weddings</Link>
+          <Link to="/services/weddings" className={`${linkStyles} text-black! ${isActive('/services/weddings') ? activeLinkStyles : ''}`}>Weddings</Link>
           </li>
           <li className='whitespace-nowrap px-3 py-4'>
-          <Link to="/services/labor-and-delivery" className={`${linkStyles} ${isActive('/services/labor-and-delivery') ? activeLinkStyles : ''}`}>Labor & Delivery</Link>
+          <Link to="/services/labor-and-delivery" className={`${linkStyles} text-black! ${isActive('/services/labor-and-delivery') ? activeLinkStyles : ''}`}>Labor & Delivery</Link>
           </li>
         </ul>
 
@@ -51,11 +53,11 @@ function Navbar() {
       {/* Login/Create Account buttons */}
       <Link
        to="/login"
-       className= "ml-12 shrink-0 inline-block px-4 py-1.5 bg-[#7E4C3C] text-white text-lg leading-tight hover:bg-[#AB8C4B] transition border-2 border-black rounded-lg"
+       className= "ml-5 shrink-0 inline-block px-4 py-1.5 bg-[#7E4C3C] text-white text-sm leading-tight hover:bg-[#AB8C4B] transition border border-black rounded-lg"
       >Log in</Link>
       <Link
        to="/signup"
-       className="ml-8 shrink-0 inline-block px-4 py-1.5 bg-[#7E4C3C] text-white text-lg leading-tight hover:bg-[#AB8C4B] transition border-2 border-black rounded-lg"
+       className="ml-4 shrink-0 inline-block px-4 py-1.5 bg-[#7E4C3C] text-white text-sm leading-tight hover:bg-[#AB8C4B] transition border border-black rounded-lg"
       >Create account</Link>
       </div>
     </nav>
