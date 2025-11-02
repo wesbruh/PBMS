61c4d758ef28a179c08694487efded21f9ec9036
emabilog <erdsmabilog@gmail.com>
2025-11-01 17:41:07 -0700

    Main: PBMS-18 Designing the Admin Dashboard View and Components which will allow an Admin user to easily access tools hosted per webpage.
    PBMS-94
    Design an Admin Dashboard View and create the basic layout
    
    PBMS-95
    Implement and create a Sidebar/Main Frame webpage structure wherein each webpage will contain a sidebar with listed options and a main frame where content will be stored.
    
    Created /src/admin directory with placefolder files and directories
    
    Created /src/admin/components directory with Sidebar and Frame components
    
    Created /src/admin/routes.js to store AdminRoutes
    
    Changed App.jsx implementation to import AdminRoutes and show different Navbar based on created routes.js file for admin users.
    
    Fixed issues regarding images --
    images and other assets within /public don't need to be accessed from root.
    
    Fixed basic Client Name Issue
    Bailey Palestini -> Baliey White

---
+++ src/admin/routes.jsx
@@ -0,0 +1,26 @@
+// src/admin/routes.jsx
+import { Outlet, Route } from 'react-router-dom';
+import AdminHome from './pages/Home/home';
+import Sessions from './pages/Sessions/sessions';
+import Contacts from './pages/Contacts/contacts';
+import Galleries from './pages/Galleries/galleries';
+import Notifications from './pages/Notifications/notifications';
+import Payments from './pages/Payments/payments';
+import Forms from './pages/Forms/forms';
+import Settings from './pages/Settings/settings';
+
+// this component only defines nested <Route>s; no <Routes> inside
+export default function AdminRoutes() {
+  return (
+    <Route path="admin" element={<Outlet />}>
+      <Route index element={<AdminHome />} />
+      <Route path="sessions" element={<Sessions />} />
+      <Route path="contacts" element={<Contacts />} />
+      <Route path="galleries" element={<Galleries />} />
+      <Route path="notifications" element={<Notifications />} />
+      <Route path="payments" element={<Payments />} />
+      <Route path="forms" element={<Forms />} />
+      <Route path="settings" element={<Settings />} />
+    </Route>
+  );
+}
