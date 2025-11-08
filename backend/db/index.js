d533b273fbff88dd9d7f3950522fc2a77942dac7
Abhijit Singh Ubhi <abhijitsinghubhi03@gmail.com>
2025-11-07 16:11:52 -0800

    Updated gitignore

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,12 +1,18 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+// Added SSL handling for connecting directly from Node to Supabase's Postgres.  
+const sslConfig = 
+  process.env.PG_SSL === "true"
+    ? {rejectUnauthorized: false }
+    : false;
 const pool = new Pool({
   host: process.env.PG_HOST,
   port: process.env.PG_PORT,
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DATABASE,
+  ssl: sslConfig, // Protects backend <-> Supabase database
 });
 
 pool.connect()
