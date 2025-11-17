c9a9c26fd5ce8a786d7b901589b11c2a343ee3d6
emabilog <erdsmabilog@gmail.com>
2025-11-16 17:40:42 -0800

    [ ADMIN-SIDE BUG FIX ] Readded `Contacts` page and sidebar option for admin dashboard

--- src/admin/routes.jsx
+++ src/admin/routes.jsx
@@ -2,6 +2,7 @@
 import { Outlet, Route } from 'react-router-dom';
 import AdminHome from './pages/Home/home';
 import Sessions from './pages/Sessions/sessions';
+import Contacts from './pages/Contacts/contacts';
 import Galleries from './pages/Galleries/galleries';
 import Notifications from './pages/Notifications/notifications';
 import Payments from './pages/Payments/payments';
@@ -14,6 +15,7 @@ export default function AdminRoutes() {
     <Route path="admin" element={<Outlet />}>
       <Route index element={<AdminHome />} />
       <Route path="sessions" element={<Sessions />} />
+      <Route path="contacts" element={<Contacts />} />
       <Route path="galleries" element={<Galleries />} />
       <Route path="notifications" element={<Notifications />} />
       <Route path="payments" element={<Payments />} />
