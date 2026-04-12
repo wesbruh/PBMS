a7c41fa4afb3774076f5015827ec3c4dc233a204
erdsmabilog <erdsmabilog@gmail.com>
2026-04-11 18:07:33 -0700

    Small Admin-Side Fixes

--- src/admin/pages/Settings/Settings.jsx
+++ src/admin/pages/Settings/Settings.jsx
@@ -442,13 +442,13 @@ function AdminSettings() {
   if (isLoadingSettings) {
     return (
       <div className="flex my-10 md:my-14 h-[65vh] mx-4 md:mx-6 lg:mx-10 bg-[#faf8f4] rounded-lg overflow-clip">
-        <div className="flex w-1/5 min-w-50 overflow-y-auto">
+        <div className="flex min-w-50 overflow-y-auto">
           <Sidebar />
         </div>
         <div className="flex h-full w-full shadow-inner rounded-lg overflow-hidden">
           <Frame>
-            <div className="relative flex flex-col bg-white p-4 w-full rounded-lg shadow-inner overflow-y-scroll">
-              <div className="p-2 text-gray-600">Loading your settings...</div>
+            <div className="w-full py-16 text-center text-brown font-serif">
+              Loading settings...
             </div>
           </Frame>
         </div>
